0f639fa50c1ad8df52eb0166d7b7ba1602835b32
nycollas <nycollasmenezes91@gmail.com>
2024-07-25 12:05:27 -0300

    segundo commit

---
+++ variaveis.js
@@ -0,0 +1,47 @@
+/*// atividade - calucular a media
+
+// //declarando variaveis 
+ let nota01 = 8;
+ let nota02 = 8;
+ let nota03 = 8;
+
+let media = (nota01 + nota02 + nota03) /3;
+
+console.log("amedia é:", media);
+
+
+// atividade - conversor de temperatura
+
+/*const celsius = 25;
+
+const fahrenheit = (celsius * 9 / 5)  + 32;
+
+console.log("a temperatura em fahrenheit é:", fahrenheit);
+*/
+
+/*
+let minutos = 1
+
+let segundos = minutos * 60;
+
+console.log(`${minutos} minutos, convertendo para segundos 
+fica: ${segundos} segundos.
+`);
+*/
+
+// atividade - calculadora simples
+
+let numero01 = 4;
+let numero02 = 5;
+
+const soma = numero01 + numero02;
+console.log(" a soma é:", soma);
+
+const subtracao = numero01 - numero02;
+console.log(" a subtração é:", subtracao);
+
+const multiplicacao = numero01 * numero02;
+console.log(" a multiplicação é:", multiplicacao);
+
+const divisao = numero01 / numero02;
+console.log(" a divisão é:", divisao);
